f99f5026f298fe05baf92acc3860a8f7f9414ee8
Francesco-Pagano <frank.pagano@protonmail.ch>
2024-11-06 16:00:12 +0100

    Red phase of US1

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -10,7 +10,8 @@ from src.parking_garage import ParkingGarageError
 class TestParkingGarage(TestCase):
 
     @patch.object(GPIO, "input")
-    def test_something(self, mock_object: Mock):
-        # This is an example of test where I want to mock the GPIO.input() function
-        pass
-
+    def test_check_occupancy(self, mock_distance_sensor: Mock):
+        mock_distance_sensor.return_value = True
+        system = ParkingGarage()
+        occupied = system.check_occupancy(system.INFRARED_PIN1)
+        self.assertTrue(occupied)
