d0990cc65baa0a47fa931704d6a3fc7086214c56
Francesco-Pagano <frank.pagano@protonmail.ch>
2024-11-06 16:49:44 +0100

    Red phase of US3-1

--- test/test_parking_garage.py
+++ test/test_parking_garage.py
@@ -33,3 +33,10 @@ class TestParkingGarage(TestCase):
         system = ParkingGarage()
         fee = system.calculate_parking_fee(datetime(2024, 11, 6, 14, 0))
         self.assertEqual(5, fee)
+
+    @patch.object(SDL_DS3231, "read_datetime")
+    def test_calculate_regular_parking_fee_different_minutes(self, mock_exit_time: Mock):
+        mock_exit_time.return_value = datetime(2024, 11, 6, 16, 1)
+        system = ParkingGarage()
+        fee = system.calculate_parking_fee(datetime(2024, 11, 6, 14, 0))
+        self.assertEqual(7.5, fee)
